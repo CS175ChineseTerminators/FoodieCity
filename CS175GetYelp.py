b5896d186fa78bda42dde8ee4cbddc961a7f37a1
tear44 <tonytear44@gmail.com>
2015-02-15 19:53:44 -0800

    Get Yelp Restaurants by City
    Needs Yelp API keys to function, located in a private script.

---
+++ CS175GetYelp.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  4 14:13:09 2015
+
+@author: Tony
+"""
+
+import rauth, tearYelpKeys
+
+# city, long, lat, rating(float)
+
+def get_search_parameters(city):
+    """Search params, will be only for restaurants in this project"""
+    params = {}
+    params["term"] = "restaurant"
+#    params["location"] = "Irvine, CA"
+    params["location"] = city
+    return params
+    
+def get_results(params):
+    """Obtain information from Yelp"""
+    consumer_key = tearYelpKeys.CKey
+    consumer_secret = tearYelpKeys.CSecret
+    token = tearYelpKeys.Token
+    token_secret = tearYelpKeys.TSecret
+    
+    session = rauth.OAuth1Session(
+        consumer_key = consumer_key,
+        consumer_secret = consumer_secret,
+        access_token = token,
+        access_token_secret = token_secret)
+        
+    request = session.get("http://api.yelp.com/v2/search", params=params)
+    
+    #Transforms the JSON API response into a Python dictionary
+    data = request.json()
+    session.close()
+    
+    return data
+    
+if __name__ == "__main__":
+    param = get_search_parameters("Irvine, CA")
+    results = get_results(param)
+    Yelp_Restaurants = results["businesses"]
+#    print(get_results(param))
