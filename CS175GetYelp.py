ce07991261643a589e80ecbc8a746b1bd73d6037
tear44 <tonytear44@gmail.com>
2015-02-23 03:43:27 -0800

    added arguments, easier for restaurants in one city
    easier on cmd line, arguments allow searching for a certain kind of
    restaurant in one city/area

--- CS175GetYelp.py
+++ CS175GetYelp.py
@@ -5,16 +5,14 @@ Created on Wed Feb  4 14:13:09 2015
 @author: Tony
 """
 
-import rauth, tearYelpKeys
+import rauth, tearYelpKeys, sys
 
-# city, long, lat, rating(float)
-
-def get_search_parameters(city):
+def get_search_parameters(area, name=''):
     """Search params, will be only for restaurants in this project"""
     params = {}
-    params["term"] = "restaurant"
-#    params["location"] = "Irvine, CA"
-    params["location"] = city
+    if (name): params["term"] = name 
+    else: params["term"] = "restaurant"
+    params["location"] = area
     return params
     
 def get_results(params):
@@ -35,14 +33,35 @@ def get_results(params):
     #Transforms the JSON API response into a Python dictionary
     data = request.json()
     session.close()
-    
     return data
     
-if __name__ == "__main__":
-    param = get_search_parameters("Irvine, CA")
+def get_restaurantList(location, restaurant):
+    """ Obtain list of restaurants in a dictionary[name]=info """
+    param = get_search_parameters(location, restaurant)
     results = get_results(param)
-    # results keys = {businesses, total, region}
-    Yelp_Restaurants = results["businesses"]
+    return results['businesses']
+
+    
+if __name__ == "__main__":
+    if (len(sys.argv)<3):
+        print("""Usage:\n\tCS175GetYelp.py "<city,state>" "<restaurant>" """)
+    else:
+        location, restaurant = sys.argv[1], sys.argv[2].replace(' ', '-')
+#        rList = get_restaurantList(location, restaurant)
+        rList = []
+        city = sys.argv[1].split(",")[0].strip()
+        total = get_restaurantList(location, restaurant)
+        for restaurant in total:
+            if (restaurant['location']['city']==city):
+                rList.append(restaurant)
+        if (len(rList)==0):
+            rList=total
+        total = None
+        print("List of restaurant dictionaries saved into var rList")
+#    Yelp_Restaurants = dict()
+#    for r in results["businesses"]:
+#        Yelp_Restaurants[r["name"]] = r
+#    Yelp_Restaurants = results["businesses"]
     # Yelp_Restaurants keys = {rating, rating_img_url, is_claimed,
     # review_count, url, rating_img_url_large, display_phone,
     # image_url, location, is_closed, categories, phone, name, url}
