f873b7a70afcaa061a4dc5dd0982fb5937d7f7f8
tear44 <tonytear44@gmail.com>
2015-02-24 02:06:14 -0800

    Update, functions to mine written
    Still needs to pick up city/restaurant data

--- CS175GetYelp.py
+++ CS175GetYelp.py
@@ -1,3 +1,4 @@
+#! /bin/usr/env python
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb  4 14:13:09 2015
@@ -7,56 +8,98 @@ Created on Wed Feb  4 14:13:09 2015
 
 import rauth, tearYelpKeys, sys
 
-def get_search_parameters(area, name=''):
-    """Search params, will be only for restaurants in this project"""
-    params = {}
-    if (name): params["term"] = name 
-    else: params["term"] = "restaurant"
-    params["location"] = area
-    return params
-    
-def get_results(params):
-    """Obtain information from Yelp"""
-    consumer_key = tearYelpKeys.CKey
-    consumer_secret = tearYelpKeys.CSecret
-    token = tearYelpKeys.Token
-    token_secret = tearYelpKeys.TSecret
-    
-    session = rauth.OAuth1Session(
-        consumer_key = consumer_key,
-        consumer_secret = consumer_secret,
-        access_token = token,
-        access_token_secret = token_secret)
+class GetYelp(object):
+
+    def __init__(self):
+        self._CKey = tearYelpKeys.CKey
+        self._CSecret = tearYelpKeys.CSecret
+        self._Token = tearYelpKeys.Token
+        self._TSecret = tearYelpKeys.TSecret
+
+    def get_search_parameters(self, area, name=''):
+        """Search params, will be only for restaurants in this project"""
+        params = {}
+        if (name): params["term"] = name 
+        else: params["term"] = "restaurant"
+        params["location"] = area
+        return params
         
-    request = session.get("http://api.yelp.com/v2/search", params=params)
-    
-    #Transforms the JSON API response into a Python dictionary
-    data = request.json()
-    session.close()
-    return data
-    
-def get_restaurantList(location, restaurant):
-    """ Obtain list of restaurants in a dictionary[name]=info """
-    param = get_search_parameters(location, restaurant)
-    results = get_results(param)
-    return results['businesses']
+    def get_results(self, params):
+        """Obtain information from Yelp"""
+        
+        session = rauth.OAuth1Session(
+            consumer_key = self._CKey,
+            consumer_secret = self._CSecret,
+            access_token = self._Token,
+            access_token_secret = self._TSecret)
+            
+        request = session.get("http://api.yelp.com/v2/search", params=params)
+        
+        #Transforms the JSON API response into a Python dictionary
+        data = request.json()
+        session.close()
+        return data
+        
+    def get_restaurantList(self, location, restaurant):
+        """ Obtain list of restaurants in a dictionary[name]=info """
+        param = self.get_search_parameters(location, restaurant)
+        results = self.get_results(param)
+        return results['businesses']     
+        
+    def filterRList(self, RList, city):
+        """ Filters RList to only restaurants with that certain City """
+        results = []
+        for r in RList:
+            if (r['location']['city']==city.strip()):
+                results.append(r)
+        return results
+
+    def read_restaurantChains(self, file=""):
+        """ Supply this function a file argument, containing all restaurant
+            that would be interesting
+        """
+        RChainList = []
+        return RChainList
+
+    def read_CityList(self, file=""):
+        """ Reads a file of cities, then returns it in a list"""
+        CityList = []
+        return CityList        
+        
+    def getRestaurantsByCity(self, restaurantList, city):
+        """ Returns a dict of all chain restaurants in city specified """
+        RC = dict()  # RC[restaurant name] = restaurant info
+        for r in restaurantList:
+            if (r in RC):
+                RC[r].append(self.filterRList(
+                            self.get_restaurantList(city.strip(), r.replace(' ','-'))))
+                            
+        return RC
+        
+    def getAllCity(self, restaurantList, cityList):
+        """ Return a dict, dict[city] = (dict[rest. name] = rest. info """
+        RCityList = []
+        for city in cityList:
+            RCityList.append(self.getRestaurantsByCity(restaurantList, city))
+        return RCityList
 
     
 if __name__ == "__main__":
     if (len(sys.argv)<3):
         print("""Usage:\n\tCS175GetYelp.py "<city,state>" "<restaurant>" """)
     else:
+        getYelp = GetYelp()
         location, restaurant = sys.argv[1], sys.argv[2].replace(' ', '-')
 #        rList = get_restaurantList(location, restaurant)
         rList = []
         city = sys.argv[1].split(",")[0].strip()
-        total = get_restaurantList(location, restaurant)
+        total = getYelp.get_restaurantList(location, restaurant)
         for restaurant in total:
             if (restaurant['location']['city']==city):
                 rList.append(restaurant)
         if (len(rList)==0):
             rList=total
-        total = None
+#        total = None
         print("List of restaurant dictionaries saved into var rList")
 #    Yelp_Restaurants = dict()
 #    for r in results["businesses"]:
